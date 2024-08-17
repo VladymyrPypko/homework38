d0537b8f7abdd5917673a2c8254e2b3b2c57173b
VladymyrPypko <zaitm99@gmail.com>
2024-08-17 18:43:59 +0300

    added bg color for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
-// import styles from './App.module.css';
+import styles from './App.module.css';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Albums from './pages/Albums/Albums';
@@ -8,15 +8,17 @@ import About from './pages/About/About';
 
 function App() {
   return (
-    <Router>
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/albums' element={<Albums></Albums>}></Route>
-        <Route path='/albums/:id' element={<Album></Album>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-      </Routes>
-    </Router>
+    <div className={styles.wrapper}>
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/albums' element={<Albums></Albums>}></Route>
+          <Route path='/albums/:id' element={<Album></Album>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
